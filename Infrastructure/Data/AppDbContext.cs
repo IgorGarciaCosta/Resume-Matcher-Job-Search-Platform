5b57466881e2d7cafffe9c68310961a73827af0e
IgorGarciaCosta <igonildo7@gmail.com>
2026-05-01 23:35:46 -0300

    refactor: remove JobApplication, JobDescription, Resume entities and their configurations

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -9,42 +9,11 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<Resume> Resumes => Set<Resume>();
-    public DbSet<JobDescription> JobDescriptions => Set<JobDescription>();
-    public DbSet<JobApplication> JobApplications => Set<JobApplication>();
     public DbSet<SavedAnalysis> SavedAnalyses => Set<SavedAnalysis>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Resume>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.FileName).HasMaxLength(256).IsRequired();
-            entity.Property(e => e.ExtractedText).IsRequired();
-        });
-
-        modelBuilder.Entity<JobDescription>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.SourceUrl).HasMaxLength(2048);
-            entity.Property(e => e.Title).HasMaxLength(512);
-            entity.Property(e => e.RawText).IsRequired();
-        });
-
-        modelBuilder.Entity<JobApplication>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.HasOne(e => e.Resume)
-                  .WithMany()
-                  .HasForeignKey(e => e.ResumeId)
-                  .OnDelete(DeleteBehavior.Cascade);
-            entity.HasOne(e => e.JobDescription)
-                  .WithMany()
-                  .HasForeignKey(e => e.JobDescriptionId)
-                  .OnDelete(DeleteBehavior.Cascade);
-        });
 
         modelBuilder.Entity<SavedAnalysis>(entity =>
         {
